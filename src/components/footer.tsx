27aaf6d7a529237af7d9db98d6fe357a2f4ca77e
Mohit <thebytesbuilder@gmail.com>
2024-11-19 21:48:26 +0530

    resolved build issue

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -97,8 +97,8 @@ export default function Footer() {
               <span className="block w-[80px] border-b-4 border-[#E84E36]"></span>
             </h3>
             <p className="text-sm text-gray-400 mb-4">
-              Don't miss to subscribe to our new feeds, kindly fill the form
-              below.
+              Don&lsquo;t miss to subscribe to our new feeds, kindly fill the
+              form below.
             </p>
             <form className="flex">
               <input
